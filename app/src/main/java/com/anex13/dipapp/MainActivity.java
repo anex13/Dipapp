db685ff4ac1d93adadd5b0cde7afd99ddfb0bd9a
anex13 <nameless13anarhy@gmail.com>
2016-03-21 20:22:10 +0300

    1

--- app/src/main/java/com/anex13/dipapp/MainActivity.java
+++ app/src/main/java/com/anex13/dipapp/MainActivity.java
@@ -1,24 +1,23 @@
 package com.anex13.dipapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.app.FragmentTransaction;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     frag1 frag1;
     frag2 frag2;
     frag3 frag3;
     frag4 frag4;
     FragmentTransaction fTrans;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +26,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-
     @SuppressWarnings("StatementWithEmptyBody")
+    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
         fTrans = getFragmentManager().beginTransaction();
 
         if (id == R.id.menuautoscan) {
-            fTrans.replace(R.id.content, frag1);
+            fTrans.add(R.id.content, frag1);
         } else if (id == R.id.menuping) {
             fTrans.replace(R.id.content, frag2);
         } else if (id == R.id.menulanscan) {
@@ -46,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        return false;
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
