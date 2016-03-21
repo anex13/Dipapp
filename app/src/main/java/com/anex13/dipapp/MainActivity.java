2ea5e8c7e089abdfb0738f244807d40ca9d0e9a4
anex13 <nameless13anarhy@gmail.com>
2016-03-21 16:43:56 +0300

    застрял
    чот не получается у меня прикрутить фрагменты к кнопкам

--- app/src/main/java/com/anex13/dipapp/MainActivity.java
+++ app/src/main/java/com/anex13/dipapp/MainActivity.java
@@ -2,12 +2,23 @@ package com.anex13.dipapp;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.app.FragmentTransaction;
+
 
 public class MainActivity extends AppCompatActivity {
+    frag1 frag1;
+    frag2 frag2;
+    frag3 frag3;
+    frag4 frag4;
+    FragmentTransaction fTrans;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +27,25 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
-
-
     }
 
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        fTrans = getFragmentManager().beginTransaction();
+
+        if (id == R.id.menuautoscan) {
+            fTrans.replace(R.id.content, frag1);
+        } else if (id == R.id.menuping) {
+            fTrans.replace(R.id.content, frag2);
+        } else if (id == R.id.menulanscan) {
+            fTrans.replace(R.id.content, frag3);
+        } else if (id == R.id.menuwiki) {
+            fTrans.replace(R.id.content, frag4);
+        }
+
+
+        return false;
+    }
 }
