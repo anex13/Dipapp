060f8857300120270512da93b217967bc2d53424
anex13 <nameless13anarhy@gmail.com>
2016-10-25 15:45:37 +0300

    сет визибилити вылетает

--- app/src/main/java/com/anex13/dipapp/FragPrefs.java
+++ app/src/main/java/com/anex13/dipapp/FragPrefs.java
@@ -13,7 +13,7 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Switch;
-import android.widget.ViewSwitcher;
+import android.widget.TextView;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -22,73 +22,97 @@ public class FragPrefs extends Fragment implements CompoundButton.OnCheckedChang
 
     public static final String AUTO_START = "autostart";
     public static final String PREF_TAG = "dipappprefs";
-    public static final String USE_MAIL ="use mail" ;
-    public static final String USE_GMAIL ="use GMail" ;
-    public static final String USE_TLS = "Star tls needed" ;
+    public static final String USE_MAIL = "use mail";
+    public static final String USE_GMAIL = "use GMail";
+    public static final String USE_TLS = "Star tls needed";
     public static final String USE_AUTH = "use smtp auth";
     public static final String PING_COUNT = "ping count";
-    private static final String MAIL_FROM = "my mail";
-    private static final String MAIL_PASS = "my pass";
-    private static final String MAIL_TO ="mail to" ;
-    private static final String MAIL_HEAD ="mail header" ;
-    private static final String MAIL_URL = "smtp server url";
-    private static final String MAIL_PORT ="smtp port" ;
-    Switch autostart, useMail, useGmail, starTLS, authNeeded;
+    public static final String MAIL_FROM = "my mail";
+    public static final String MAIL_PASS = "my pass";
+    public static final String MAIL_TO = "mail to";
+    public static final String MAIL_HEAD = "mail header";
+    public static final String MAIL_URL = "smtp server url";
+    public static final String MAIL_PORT = "smtp port";
+    public static final String USE_WIFI = "wifi only";
+    public static final String USE_CHARGE = "on charge only";
+    Switch autostart, useMail, useGmail, starTLS, authNeeded, wifionly, oncharge;
     SeekBar pingcount;
     SharedPreferences sPref;
     EditText email, password, smtpurl, smtpPort, mailto, mailheader;
-    ViewSwitcher wsapi, wsmail, wsgmail;
     Button mailSave;
+    TextView gmailref, tvset;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.prefs_frag, container, false);
         sPref = getActivity().getSharedPreferences(PREF_TAG, MODE_PRIVATE);
+        gmailref = (TextView) rootView.findViewById(R.id.gmail_ref);
+        tvset = (TextView) rootView.findViewById(R.id.tv_email_set);
         autostart = (Switch) rootView.findViewById(R.id.swonboot);
-        autostart.setChecked(sPref.getBoolean(AUTO_START,false));
+        autostart.setChecked(sPref.getBoolean(AUTO_START, false));
         autostart.setOnCheckedChangeListener(this);
-        useMail= (Switch) rootView.findViewById(R.id.sw_email);
+        useMail = (Switch) rootView.findViewById(R.id.sw_email);
         useMail.setOnCheckedChangeListener(this);
-        useMail.setChecked(sPref.getBoolean(USE_MAIL,false));
+        useMail.setChecked(sPref.getBoolean(USE_MAIL, false));
         useGmail = (Switch) rootView.findViewById(R.id.sw_gmail);
         useGmail.setOnCheckedChangeListener(this);
-        useGmail.setChecked(sPref.getBoolean(USE_GMAIL,false));
+        useGmail.setChecked(sPref.getBoolean(USE_GMAIL, false));
         starTLS = (Switch) rootView.findViewById(R.id.sw_tls);
         starTLS.setOnCheckedChangeListener(this);
-        starTLS.setChecked(sPref.getBoolean(USE_TLS,false));
+        starTLS.setChecked(sPref.getBoolean(USE_TLS, false));
         authNeeded = (Switch) rootView.findViewById(R.id.sw_auth);
-        authNeeded.setChecked(sPref.getBoolean(USE_AUTH,false));
+        authNeeded.setChecked(sPref.getBoolean(USE_AUTH, false));
         authNeeded.setOnCheckedChangeListener(this);
-        mailSave=(Button) rootView.findViewById(R.id.mailprefssave);
+        wifionly = (Switch) rootView.findViewById(R.id.sw_wifi_only);
+        wifionly.setChecked(sPref.getBoolean(USE_WIFI, false));
+        wifionly.setOnCheckedChangeListener(this);
+        oncharge = (Switch) rootView.findViewById(R.id.sw_on_charge);
+        oncharge.setChecked(sPref.getBoolean(USE_CHARGE, false));
+        oncharge.setOnCheckedChangeListener(this);
+        mailSave = (Button) rootView.findViewById(R.id.mailprefssave);
         mailSave.setOnClickListener(this);
-        pingcount=(SeekBar) rootView.findViewById(R.id.pingcount);
+        pingcount = (SeekBar) rootView.findViewById(R.id.pingcount);
         pingcount.setOnSeekBarChangeListener(this);
-        pingcount.setProgress(sPref.getInt(PING_COUNT,3));
-        email=(EditText) rootView.findViewById(R.id.edit_email);
-        email.setText(sPref.getString(MAIL_FROM,""));
-        password=(EditText) rootView.findViewById(R.id.edit_pass);
-        password.setText(sPref.getString(MAIL_PASS,""));
-        mailto=(EditText) rootView.findViewById(R.id.edit_mailto);
-        mailto.setText(sPref.getString(MAIL_TO,""));
-        mailheader=(EditText) rootView.findViewById(R.id.edit_mailheader);
-        mailheader.setText(sPref.getString(MAIL_HEAD,""));
-        smtpurl=(EditText) rootView.findViewById(R.id.edit_smtpsrv);
-        smtpurl.setText(sPref.getString(MAIL_URL,""));
-        smtpPort=(EditText) rootView.findViewById(R.id.edit_smtpport);
-        smtpPort.setText(sPref.getString(MAIL_PORT,""));
-        wsapi= (ViewSwitcher) rootView.findViewById(R.id.wsJsheduler);
-        wsmail= (ViewSwitcher) rootView.findViewById(R.id.ws_email);
-        wsgmail= (ViewSwitcher) rootView.findViewById(R.id.ws_Gmail);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            wsapi.showNext();
-                    }
-       // if (sPref.getBoolean(USE_MAIL,false)){
-        //    wsmail.showNext();
-       //     if (sPref.getBoolean(USE_GMAIL,false))
-       //         wsgmail.showNext();
-      //  }
-
+        pingcount.setProgress(sPref.getInt(PING_COUNT, 3));
+        email = (EditText) rootView.findViewById(R.id.edit_email);
+        email.setText(sPref.getString(MAIL_FROM, ""));
+        password = (EditText) rootView.findViewById(R.id.edit_pass);
+        password.setText(sPref.getString(MAIL_PASS, ""));
+        mailto = (EditText) rootView.findViewById(R.id.edit_mailto);
+        mailto.setText(sPref.getString(MAIL_TO, ""));
+        mailheader = (EditText) rootView.findViewById(R.id.edit_mailheader);
+        mailheader.setText(sPref.getString(MAIL_HEAD, ""));
+        smtpurl = (EditText) rootView.findViewById(R.id.edit_smtpsrv);
+        smtpurl.setText(sPref.getString(MAIL_URL, ""));
+        smtpPort = (EditText) rootView.findViewById(R.id.edit_smtpport);
+        smtpPort.setText(sPref.getString(MAIL_PORT, ""));
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            oncharge.setVisibility(View.GONE);
+            wifionly.setVisibility(View.GONE);
+        }
+        if (!sPref.getBoolean(USE_MAIL, false)) {
+            tvset.setVisibility(View.GONE);
+            useGmail.setVisibility(View.GONE);
+            email.setVisibility(View.GONE);
+            password.setVisibility(View.GONE);
+            gmailref.setVisibility(View.GONE);
+            smtpurl.setVisibility(View.GONE);
+            smtpPort.setVisibility(View.GONE);
+            authNeeded.setVisibility(View.GONE);
+            starTLS.setVisibility(View.GONE);
+            mailto.setVisibility(View.GONE);
+            mailheader.setVisibility(View.GONE);
+            mailSave.setVisibility(View.GONE);
+        } else {
+            if (sPref.getBoolean(USE_GMAIL, false)) {
+                smtpurl.setVisibility(View.GONE);
+                smtpPort.setVisibility(View.GONE);
+                authNeeded.setVisibility(View.GONE);
+                starTLS.setVisibility(View.GONE);
+            } else
+                gmailref.setVisibility(View.GONE);
+        }
 
 
         return rootView;
@@ -104,16 +128,17 @@ public class FragPrefs extends Fragment implements CompoundButton.OnCheckedChang
                 ed.apply();
                 break;
             case R.id.sw_email:
-                wsmail.showNext();
                 ed = sPref.edit();
                 ed.putBoolean(USE_MAIL, buttonView.isChecked());
                 ed.apply();
+                setVisibilityMail();
+                setVisibilityGmail();
                 break;
             case R.id.sw_gmail:
-                wsgmail.showNext();
                 ed = sPref.edit();
                 ed.putBoolean(USE_GMAIL, buttonView.isChecked());
                 ed.apply();
+                setVisibilityGmail();
                 break;
             case R.id.sw_auth:
                 break;
@@ -125,6 +150,48 @@ public class FragPrefs extends Fragment implements CompoundButton.OnCheckedChang
 
     }
 
+    private void setVisibilityMail() {
+        if (!sPref.getBoolean(USE_MAIL, false)) {
+            tvset.setVisibility(View.GONE);
+            useGmail.setVisibility(View.GONE);
+            email.setVisibility(View.GONE);
+            password.setVisibility(View.GONE);
+            gmailref.setVisibility(View.GONE);
+            smtpurl.setVisibility(View.GONE);
+            smtpPort.setVisibility(View.GONE);
+            authNeeded.setVisibility(View.GONE);
+            starTLS.setVisibility(View.GONE);
+            mailto.setVisibility(View.GONE);
+            mailheader.setVisibility(View.GONE);
+            mailSave.setVisibility(View.GONE);
+        } else {
+            tvset.setVisibility(View.VISIBLE);
+            useGmail.setVisibility(View.VISIBLE);
+            email.setVisibility(View.VISIBLE);
+            password.setVisibility(View.VISIBLE);
+            mailto.setVisibility(View.VISIBLE);
+            mailheader.setVisibility(View.VISIBLE);
+            mailSave.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void setVisibilityGmail() {
+        if (sPref.getBoolean(USE_MAIL, false)) {
+            if (sPref.getBoolean(USE_GMAIL, false)) {
+                gmailref.setVisibility(View.VISIBLE);
+                smtpurl.setVisibility(View.GONE);
+                smtpPort.setVisibility(View.GONE);
+                authNeeded.setVisibility(View.GONE);
+                starTLS.setVisibility(View.GONE);
+            } else {
+                gmailref.setVisibility(View.GONE);
+                smtpurl.setVisibility(View.VISIBLE);
+                smtpPort.setVisibility(View.VISIBLE);
+                authNeeded.setVisibility(View.VISIBLE);
+                starTLS.setVisibility(View.VISIBLE);
+            }
+        }
+    }
 
     @Override
     public void onClick(View v) {
