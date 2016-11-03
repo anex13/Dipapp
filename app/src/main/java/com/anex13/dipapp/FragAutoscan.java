de3fe4ae93af2011578eb5d979fb3a50ceafaf4e
anex13 <nameless13anarhy@gmail.com>
2016-11-03 16:02:34 +0300

    чот потерялось

--- app/src/main/java/com/anex13/dipapp/FragAutoscan.java
+++ app/src/main/java/com/anex13/dipapp/FragAutoscan.java
@@ -1,5 +1,6 @@
 package com.anex13.dipapp;
 
+import android.app.Activity;
 import android.content.ContentUris;
 import android.database.Cursor;
 import android.net.Uri;
@@ -16,29 +17,56 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
 
-public class FragAutoscan extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>, View.OnClickListener {
-    private static final int MENU_EDIT_SERVER =1 ;
-    private static final int MENU_DEL_SERVER =2 ;
-    private static final String LOGTAG ="my log" ;
+public class FragAutoscan extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>, View.OnClickListener  {
+    private static final int MENU_EDIT_SERVER = 1;
+    private static final int MENU_DEL_SERVER = 2;
+    private static final String LOGTAG = "my log";
+    private Animation fabOpenAnimation;
+    private Animation fabCloseAnimation;
+    private static boolean menuisopen = false;
     ListView lv;
     Server menusrv;
-    FragSRVAdd editfrag=new FragSRVAdd();
-    Bundle args=new Bundle();
+    FragSRVAdd editfrag = new FragSRVAdd();
+    Bundle args = new Bundle();
+    FloatingActionButton fabmain, fabadd, fabrefresh, stopalrm;
+    View viewadd,viewrefresh,viewstop;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.autoscan, container, false);
-        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
+
+        fabmain = (FloatingActionButton) rootView.findViewById(R.id.fab);
+        fabadd = (FloatingActionButton) rootView.findViewById(R.id.fabaddsrv);
+        fabrefresh = (FloatingActionButton) rootView.findViewById(R.id.fabrefresh);
+        stopalrm = (FloatingActionButton) rootView.findViewById(R.id.fabstopalrm);
+        fabadd.setOnClickListener(this);
+        fabrefresh.setOnClickListener(this);
+        stopalrm.setOnClickListener(this);
+        viewadd= (View) rootView.findViewById(R.id.fabaddgroup);
+        viewrefresh= (View) rootView.findViewById(R.id.fabrefreshgroup);
+        viewstop= (View) rootView.findViewById(R.id.fabstopalrmgroup);
+        viewadd.setVisibility(View.INVISIBLE);
+        viewrefresh.setVisibility(View.INVISIBLE);
+        viewstop.setVisibility(View.INVISIBLE);
         lv = (ListView) rootView.findViewById(R.id.srvlist);
-        Button btn = (Button) rootView.findViewById(R.id.buttonchkitnow);
-        btn.setOnClickListener(this);
-        fab.setOnClickListener(this);
+        menuisopen=false;
+        fabmain.setOnClickListener(this);
         registerForContextMenu(lv);
+        getAnimations();
         return rootView;
+    }
+
+    private void getAnimations() {
+
+        fabOpenAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.open_fab_menu);
+
+        fabCloseAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.close_fab_menu);
+
     }
 
     @Override
@@ -69,19 +97,19 @@ public class FragAutoscan extends Fragment implements LoaderManager.LoaderCallba
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-        Cursor c  = ((Cursor) lv.getAdapter().getItem(info.position));
-        menusrv=new Server(c);
-        String name  = menusrv.getName();
+        Cursor c = ((Cursor) lv.getAdapter().getItem(info.position));
+        menusrv = new Server(c);
+        String name = menusrv.getName();
         menu.setHeaderTitle(name);
-        menu.add(Menu.NONE, MENU_EDIT_SERVER, Menu.NONE,"Edit server");
-        menu.add(Menu.NONE, MENU_DEL_SERVER, Menu.NONE,"Delete server");
+        menu.add(Menu.NONE, MENU_EDIT_SERVER, Menu.NONE, "Edit server");
+        menu.add(Menu.NONE, MENU_DEL_SERVER, Menu.NONE, "Delete server");
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case MENU_EDIT_SERVER:
-                args= menusrv.toBundle();
+                args = menusrv.toBundle();
                 editfrag.setArguments(args);
                 ((MainActivity) getActivity()).showFragment(editfrag, true);
                 return true;
@@ -89,8 +117,8 @@ public class FragAutoscan extends Fragment implements LoaderManager.LoaderCallba
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                      Uri uri = ContentUris.withAppendedId(SRVContentProvider.SERVERS_CONTENT_URI, menusrv.getId());
-                        getActivity().getContentResolver().delete(uri,null,null);
+                        Uri uri = ContentUris.withAppendedId(SRVContentProvider.SERVERS_CONTENT_URI, menusrv.getId());
+                        getActivity().getContentResolver().delete(uri, null, null);
                     }
                 }).start();
                 return true;
@@ -103,12 +131,37 @@ public class FragAutoscan extends Fragment implements LoaderManager.LoaderCallba
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.fab:
+                if (!menuisopen)
+                    openMenu();
+                else
+                    closeMenu();
+                break;
+            case R.id.fabaddsrv:
                 ((MainActivity) getActivity()).showFragment(new FragSRVAdd(), true);
                 break;
-            case R.id.buttonchkitnow:
+            case R.id.fabrefresh:
                 IntentSrvs.checkItNow(getContext());
+                break;
+            case R.id.fabstopalrm:
+                IntentSrvs.cancelALRM(getContext());
                 break;
         }
     }
+
+
+    public void openMenu() {
+        viewadd.startAnimation(fabOpenAnimation);
+        viewrefresh.startAnimation(fabOpenAnimation);
+        viewstop.startAnimation(fabOpenAnimation);
+        menuisopen = true;
+    }
+
+    public void closeMenu() {
+        viewadd.startAnimation(fabCloseAnimation);
+        viewrefresh.startAnimation(fabCloseAnimation);
+        viewstop.startAnimation(fabCloseAnimation);
+        menuisopen=false;
+    }
+
 }
 // TODO: 20.10.2016 пулл ту рефрэш , разобраться с эдит срв , отображать время след алярма и состояние проверки, покрутить лайаут 
